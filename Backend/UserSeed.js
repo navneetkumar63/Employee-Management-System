f05ed30415bf2ed820be7b1f0590eca1bb3d9790
navneetkumar63 <158578309+navneetkumar63@users.noreply.github.com>
2025-09-18 16:52:07 +0530

    Update UserSeed.js

--- Backend/UserSeed.js
+++ Backend/UserSeed.js
@@ -11,8 +11,8 @@ const userRegister = async () =>{
     try{
         const hashPassword = await bcrypt.hash('admin',10);
   const newUser = new User({
-    name:"Admin",
-    email:'admin@gmail.com',
+    name:"Adm",
+    email:'adm.com',
     password:hashPassword,
  role:'admin'
 })
